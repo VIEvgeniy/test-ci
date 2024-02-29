5ba992e412eff7d23fe19c00df3a6168a53f7919
VIEvgeniy <vakevg@yandex.ru>
2024-02-29 17:56:02 +0300

    modified:   package.json 	modified:   src/__tests__/character.js 	modified:   src/character.js

--- src/__tests__/character.js
+++ src/__tests__/character.js
@@ -1,9 +1,9 @@
-import CharacterInfo from '../character';
+import { characterInfo } from '../character';
 
 test.each([
     [{name: 'мечник', health: 10},  'critical'],    
     [{name: 'маг', health: 100},  'healthy'],
     [{name: 'лучник', health: 40}, 'wounded']
 ])('test caracter %s',(caracter, expected) => {
-    expect(CharacterInfo(caracter)).toBe(expected);
+    expect(characterInfo(caracter)).toBe(expected);
 });
--- src/character.js
+++ src/character.js
@@ -1,5 +1,5 @@
-export default function characterInfo(character) {
+export function characterInfo(character) {
     if (character.health > 50) return 'healthy';
     if (character.health > 15) return 'wounded';
     return 'critical';
-}
+};
